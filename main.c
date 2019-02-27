958bbef26756557ef22218df3ed084f174f41b3c
Vincent MUCCHIELLI <vimucchi@e1r7p14.42.fr>
2019-02-27 17:48:14 +0100

    adding zoom

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vimucchi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 19:55:09 by vimucchi          #+#    #+#             */
-/*   Updated: 2019/02/27 16:39:57 by sboulaao         ###   ########.fr       */
+/*   Updated: 2019/02/27 17:47:47 by vimucchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,38 @@ void		ft_exit(void)
 
 int		keyboard(int key, t_mlx *mlx)
 {
+
 	//t_space		add;
+
+	printf("Key:%d\n", key);
 	if (key == 53)
 	{
-		mlx_clear_window(mlx->ptr, mlx->wdw);
+		mlx_destroy_window(mlx->ptr, mlx->wdw);
 		exit(1);
+	}
+	if (key == 2)
+	{
+		mlx_clear_window(mlx->ptr, mlx->wdw);
 	}
 	if (key == 38 || key == 69 || key == 24)
 	{
 		mlx->check++;
-	//	add = ft_new_space(0, 0, 0, 2.0);
-		printf("%d\n", mlx->zoom->gap_x);
-		mlx->zoom->gap_x *= 1.2;
-		printf("%d\n", mlx->zoom->gap_x);
-		mlx->zoom->gap_y *= 1.2;
-		mlx->zoom->gap_z *= 1.2;
-		printf("%d\n", mlx->img->data[5]);
-		ft_bzero(mlx->img->data, mlx->img->s_l);
-		printf("%d\n", mlx->img->data[5]);
+		mlx->zoom->gap_x *= 1.1;
+		mlx->zoom->gap_y *= 1.1;
+		ft_bzero(mlx->img->data, WIN_WIDTH * WIN_HEIGHT * sizeof(int));
+		mlx_clear_window(mlx->ptr, mlx->wdw);
 		ft_draw_map(mlx, 'i');
+		mlx_put_image_to_window(mlx->ptr, mlx->wdw, mlx->img->img_ptr, 0, 0);
 	}
 	if (key == 40 || key == 27 || key == 78)
 	{
 		mlx->check++;
-	//	add = ft_new_space(0, 0, 0, -2.0);
-		mlx->zoom->gap_x *= 0.2;
-		mlx->zoom->gap_y *= 0.2;
-		mlx->zoom->gap_z *= 0.2;
-		ft_bzero(mlx->img->data, mlx->img->s_l);
+		mlx->zoom->gap_x *= 0.9;
+		mlx->zoom->gap_y *= 0.9;
+		mlx_clear_window(mlx->ptr, mlx->wdw);
+		ft_bzero(mlx->img->data, WIN_WIDTH * WIN_HEIGHT * sizeof(int));
 		ft_draw_map(mlx, 'i');
+		mlx_put_image_to_window(mlx->ptr, mlx->wdw, mlx->img->img_ptr, 0, 0);
 	}
 	return (0);
 }
@@ -75,6 +78,7 @@ int			main(int ac, char **av)
 	int		fd;
 	t_mlx	mlx[1];
 	t_img	img[1];
+	t_zoom	zoom;
 
 	if (ac == 3)
 	{
@@ -94,6 +98,7 @@ int			main(int ac, char **av)
 		printf("hello\n");
 		mlx->check = 0;
 		printf("hello check\n");
+		mlx->zoom = &zoom;
 		ft_draw_map(mlx, av[2][1]);
 		mlx_put_image_to_window(mlx->ptr, mlx->wdw, img->img_ptr, 0, 0);
 		mlx_key_hook(mlx->wdw, keyboard, mlx);
